63671ab8e75e5ef2406d0c9b426c3b538a86ab00
tamnhu <nguyenthitamnhu220599@gmail.com>
2019-11-14 12:35:13 +0700

    Revert "Commit"
    This reverts commit a12e095a8cb142ee0156d865d946445ab9cee21e.

--- 2/js/scripts.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $('.navbar-default .dmenu').hover(function() {
-        $(this).find('.sm-menu').first().stop(true, true).slideDown(150);
-    }, function() {
-        $(this).find('.sm-menu').first().stop(true, true).slideUp(100);
-    });
-});
